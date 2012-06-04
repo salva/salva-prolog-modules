bc7b2881f1cd1d5e906e4bef613d09ae36b1dba3
Salvador Fandino <sfandino@yahoo.com>
2012-06-04 14:09:55 +0200

    add copyright and better name for lcs_dumb

--- lcs.pl
+++ lcs.pl
@@ -1,12 +1,35 @@
 % -*- Mode: Prolog -*- 
 
+/** <module> LCS: Longest common subsequence
+
+  Copyright (C) 2012 by Salvador Fandiño
+
+  This library is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with PrologDoc; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  US
+
+  @author Salvador Fandiño
+  @license GPL
+  
+*/
+
 :- module(lcs, [lcs/3, lcs/4]).
 
 :- use_module(hashtable).
 
 lcs(A, B, O) :- lcs(A, B, O, _).
 
-lcs_simple(A, B, O, Len) :-
+lcs_dumb(A, B, O, Len) :-
         (   A = [HA|TA],
             B = [HB|TB]
         ->  (   HA = HB
